e4d5b898bb21e5652fd84e31c52c513247aa682e
zhan wang <zhanzw@gmail.com>
2015-05-06 00:32:25 +0100

    enabled multi-digit entry in 1st num

--- js-calc-it/js/calc.js
+++ js-calc-it/js/calc.js
@@ -1,18 +1,42 @@
 
 //display number on click
-var number1 = ;
-var number2 = ;
-var opSign = ;
-var result = ;
-
+var numX = [];
+var opSign = [];
+var result;
+var allDigits = [];
 
 var inputNum = document.getElementsByClassName("number");
 
+var addition = function (numX) {
+	return (Number(numX[0]) + Number(numX[1])); 
+}
+
+var calFunc = function (opSign) {
+	return (opSign);
+}
+
 for (i=0; i<inputNum.length; i++) {
 	inputNum[i].addEventListener('click', function(e){
-		console.log(this.value);
-	})
+		if (opSign.length < 1) {
+			numX.push(this.value);
+			var allDigits = [numX.join('')];
+		} else {
+			numX.push(this.value);
+		}
+
+		
+
+		console.log(numX);
+		console.log(allDigits);
+
+
+		console.log(addition(numX));
+	});
+	
 }
+
+
+
 
 
 
@@ -20,14 +44,27 @@ var inputOp = document.getElementsByClassName("operator");
 
 for (j=0; j<inputOp.length; j++) {
 	inputOp[j].addEventListener ('click', function(e) {
-		console.log(this.value);
+		opSign.push(this.value);
+		console.log(opSign);
 	})
 }
 
 
-var equal = document.getElementsByClassName("eq");
+// if (opSign === '+') {
+// 	console.log(numX[0] + numX[1]);
+// 	alert(numX[0] + numX[1]);
+// }
 
-equal[0].addEventListener('click', function(e) {
-	console.log(this.value);
-});
+
+
+// console.log(numX[0] + numX[1]);
+
+// var equal = document.getElementsByClassName("eq");
+
+// equal[0].addEventListener('click', function(e) {
+// 	console.log(this.value);
+// 	return this.value;
+// 	console.log(numX[0] + numX[1]);
+// });
+
 
