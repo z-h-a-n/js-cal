3b9df6a86ba8f43d0a901d5ba5550a5ddd594116
zhan wang <zhanzw@gmail.com>
2015-05-06 09:41:27 +0100

    created 3 arrays for num1, num2 and opSign

--- js-calc-it/js/calc.js
+++ js-calc-it/js/calc.js
@@ -1,53 +1,79 @@
 
-//display number on click
-var numX = [];
+
+
+//only works in console.log
+//only single digit and + works at the moment
+//will use num.join to put multiple digits together using a conditional statement
+
+// var numX = [];
+var num1 = [];
+var num2 = [];
 var opSign = [];
 var result;
-var allDigits = [];
+
 
 var inputNum = document.getElementsByClassName("number");
 
-var addition = function (numX) {
-	return (Number(numX[0]) + Number(numX[1])); 
-}
+
+var addition = function (num1, num2) {
+return (Number(num1[0]) + Number(num2[0])); 
+};
+
+var subtract = function(num1, num2) {
+ return(Number(num1[0]) - Number(num2[0])); 
+};
+
+var multiply = function(num1, num2) {
+ return(Number(num1[0]) * Number(num2[0])); 
+};
+
+var division = function(num1, num2) {
+  return(Number(num1[0]) / Number(num2[0])); 
+};
+
 
 var calFunc = function (opSign) {
 	return (opSign);
 }
 
+
+
 for (i=0; i<inputNum.length; i++) {
 	inputNum[i].addEventListener('click', function(e){
-		if (opSign.length < 1) {
-			numX.push(this.value);
-			var allDigits = [numX.join('')];
-		} else {
-			numX.push(this.value);
-		}
+	if (opSign.length < 1) {
+		num1.push(this.value);
+		console.log(num1);
+	} else {
+		num2.push(this.value);
+		console.log(num2);
+	}
 
-		
-
-		console.log(numX);
-		console.log(allDigits);
-
-
-		console.log(addition(numX));
 	});
-	
 }
-
-
-
-
 
 
 var inputOp = document.getElementsByClassName("operator");
 
 for (j=0; j<inputOp.length; j++) {
 	inputOp[j].addEventListener ('click', function(e) {
-		opSign.push(this.value);
-		console.log(opSign);
+
+	if (opSign.length < 1) {
+	opSign.push(this.value);
+	console.log(opSign);
+	}
+		
+	
 	})
 }
+
+
+var equal = document.getElementsByClassName("eq");
+
+equal[0].addEventListener('click', function(e) {
+	console.log(this.value);
+	console.log(addition(num1, num2));
+});
+
 
 
 // if (opSign === '+') {
@@ -67,4 +93,23 @@ for (j=0; j<inputOp.length; j++) {
 // 	console.log(numX[0] + numX[1]);
 // });
 
+// for (i=0; i<inputNum.length; i++) {
+// 	inputNum[i].addEventListener('click', function(e){
+// 		// if (opSign.length < 1) {
+// 			numX.push(this.value);
+// 		// 	var allDigits = [numX.join('')];
+// 		// } else {
+// 		// 	numX.push(this.value);
+// 		// }
+
+		
+
+// 		// console.log(numX);
+// 		// console.log(allDigits);
+
+
+// 		// console.log(addition(numX));
+// 	});
+	
+// }
 
